ec9a68e2ebc4736d2d651547ca6f10e87c231c89
subinjose11 <subinjose911@gmail.com>
2024-11-22 23:14:25 +0530

    feat: log in page design

--- lib/feature/auth/presentation/ui/log_in_page.dart
+++ lib/feature/auth/presentation/ui/log_in_page.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
 import 'package:auto_route/auto_route.dart';
 import 'package:chat_app/core/styles/text_styles.dart';
+import 'package:chat_app/core/widget/custom_button.dart';
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -9,26 +12,83 @@ class LogInPage extends StatefulWidget {
   @override
   State<LogInPage> createState() => _LogInPageState();
 }
-
 class _LogInPageState extends State<LogInPage> {
+   final phoneController = TextEditingController();
+  Country? country;
+
+  @override
+  void dispose() {
+    super.dispose();
+    phoneController.dispose();
+  }
+
+   void pickCountry() {
+     showCountryPicker(
+       context: context,
+       showPhoneCode: true,
+       onSelect: (Country selectedCountry) {
+         setState(() {
+           country = selectedCountry;
+           log("Selected Country: ${country!.displayName}");
+         });
+       },
+     );
+   }
+
+  void sendPhoneNumber() {
+    String phoneNumber = phoneController.text.trim();
+    if (country != null && phoneNumber.isNotEmpty) {
+      // ref.read(authControllerProvider)
+      //     .signInWithPhone(context, '+${country!.phoneCode}$phoneNumber');
+    } else {
+      // showSnackBar(context: context, content: 'Fill out all the fields');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size=MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
          title:  Text('Enter your phone number',style: subText16M,),
         elevation: 0,
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(18),
-          child: Column(
-            children: [
-              TextButton(
-                    onPressed: (){},
-                    child: const Text('Pick Country'),
+      body: Padding(
+        padding: const EdgeInsets.all(18),
+        child: Column(
+          children: [
+              const Text('ChatApp will need to verify your phone number.'),
+            const SizedBox(height: 10),
+            TextButton(
+                  onPressed: (){pickCountry();},
+                  child: const Text('Pick Country'),
+                ),
+                 const SizedBox(height: 5),
+            Row(
+              children: [
+               if (country != null) Text('+${country!.phoneCode}'),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: size.width * 0.7,
+                  child: TextField(
+                    controller: phoneController,
+                    keyboardType:TextInputType.number ,
+                    decoration: const InputDecoration(
+                      hintText: 'phone number',
+                    ),
                   ),
-            ],
-          ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            SizedBox(
+              width: 90,
+              child: CustomButton(
+                onPressed: (){},
+                text: 'NEXT',
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:chat_app/core/theme/app_theme.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/routes/app_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-/// Ensure this is initialized once and reused
-final _appRouter = AppRouter();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,22 +14,25 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
   Widget build(BuildContext context) {
+    final appRouter=ref.watch(appRouterProvider);
     return MaterialApp.router(
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme: lightTheme, // Replace with your custom light theme
       darkTheme: darkTheme, // Replace with your custom dark theme
       themeMode: ThemeMode.system, // Automatically switches based on system theme
-      routerDelegate: AutoRouterDelegate(
-        _appRouter,
-        navigatorObservers: () => [AutoRouteObserver()],
-      ),
-      routeInformationParser: _appRouter.defaultRouteParser(), // Parse route info
+       routerConfig: appRouter.config(),
     );
   }
 }
+final appRouterProvider=Provider<AppRouter>((ref)=>AppRouter());
