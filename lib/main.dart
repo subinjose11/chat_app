894503d0752c0353427f5b004bb435868ed1dc67
subinjose11 <subinjose911@gmail.com>
2024-11-24 14:28:32 +0530

    feat: supabase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
+import 'package:chat_app/core/styles/constants.dart';
 import 'package:chat_app/core/theme/app_theme.dart';
 import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/routes/app_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+ 
+  await Supabase.initialize(
+    url: supaBaseUrl,
+    anonKey:supaBaseAnonKey
+  );
+ await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -24,15 +30,17 @@ class MyApp extends ConsumerStatefulWidget {
 class _MyAppState extends ConsumerState<MyApp> {
   @override
   Widget build(BuildContext context) {
-    final appRouter=ref.watch(appRouterProvider);
+    final appRouter = ref.watch(appRouterProvider);
     return MaterialApp.router(
       title: 'Chat App',
       debugShowCheckedModeBanner: false,
       theme: lightTheme, // Replace with your custom light theme
       darkTheme: darkTheme, // Replace with your custom dark theme
-      themeMode: ThemeMode.system, // Automatically switches based on system theme
-       routerConfig: appRouter.config(),
+      themeMode:
+          ThemeMode.system, // Automatically switches based on system theme
+      routerConfig: appRouter.config(),
     );
   }
 }
-final appRouterProvider=Provider<AppRouter>((ref)=>AppRouter());
+
+final appRouterProvider = Provider<AppRouter>((ref) => AppRouter());
