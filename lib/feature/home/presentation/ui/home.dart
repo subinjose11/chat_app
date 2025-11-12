e1272d7df6b7d1cd217e46d42100b8db41aff029
subinjose11 <subinjose911@gmail.com>
2025-11-12 09:06:58 +0530

    refact: unnessary feat removed

--- lib/feature/home/presentation/ui/home.dart
+++ lib/feature/home/presentation/ui/home.dart
@@ -1,9 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:chat_app/feature/account/presentation/ui/account_screen.dart';
-import 'package:chat_app/feature/chat/presentation/ui/chat_list_screen.dart';
 import 'package:chat_app/feature/home/presentation/controller/home_controller.dart';
 import 'package:chat_app/feature/home/presentation/widget/bottom_nav_bar.dart';
-import 'package:chat_app/feature/status/presentation/ui/status_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -25,8 +23,8 @@ class _MyHomePageState extends ConsumerState<HomePage> {
   }
 
   final screens = [
-    const ChatListScreen(),
-    const StatusScreen(),
+    Text('Chats'),
+    Text('Status'),
     const AccountScreen(),
   ];
 }
